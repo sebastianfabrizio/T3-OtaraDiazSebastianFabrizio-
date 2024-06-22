791b19edf440f9f81691c19a09e29dcccae991f0
unknown <SUITE@SUITEBE308-ST04.cibertec.edu.pe>
2024-06-22 13:31:04 -0500

    Ejercicio resuelto añadiendo la logica y el FormsModule

--- src/app/dashboard/ejercicio1/ejercicio1.component.ts
+++ src/app/dashboard/ejercicio1/ejercicio1.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 import { MaterialModule } from '../../angular-material/material/material.module';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-ejercicio1',
   standalone: true,
-  imports: [MaterialModule],
+  imports: [MaterialModule, FormsModule],
   templateUrl: './ejercicio1.component.html',
   styleUrl: './ejercicio1.component.css'
 })
 export class Ejercicio1Component {
+mostrarM=false
+resultado="esperando data"
+  frmMonto={
+    hora:null,
+    minuto:null
+  }
 
+  calcularMonto(){
+    let costoH = Number(this.frmMonto.hora) * 1500
+    let min = Number(this.frmMonto.minuto)
+    let costoM = (1500 * min) / 60
+    let total = costoH+costoM
+this.resultado= "Usted pagará " + total
+this.mostrarM=true
+  }
 }
