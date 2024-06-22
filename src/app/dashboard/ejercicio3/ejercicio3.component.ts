bf31caeb22db15f7a807863b46cddabbd8f4bf2c
unknown <SUITE@SUITEBE308-ST04.cibertec.edu.pe>
2024-06-22 14:13:37 -0500

    Ejercicio 3 acabado

--- src/app/dashboard/ejercicio3/ejercicio3.component.ts
+++ src/app/dashboard/ejercicio3/ejercicio3.component.ts
@@ -11,4 +11,28 @@ import { MaterialModule } from '../../angular-material/material/material.module'
 })
 export class Ejercicio3Component {
 
+  resultado = "Esperando data";
+  mostrarM = false;
+  frmMonto = {
+    cantidad: null,
+    precio: null
+  };
+
+  calcularMonto() {
+    let can = Number(this.frmMonto.cantidad);
+    let pre = Number(this.frmMonto.precio);
+    var total;
+
+    if (can > 20) {
+      total = can * pre * 0.9;
+    } else if (can <= 20 && can > 10) {
+      total = can * pre * 0.95;
+    } else {
+      total = can * pre;
+    }
+
+    this.resultado = "Usted pagará " + total.toFixed(2); 
+    this.mostrarM = true;
+  }
+
 }
